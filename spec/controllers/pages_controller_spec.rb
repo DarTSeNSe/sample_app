6d2f41477a4f61e924a9285c0514fd473b98bd94
DarTSeNSe <sense8life@gmail.com>
2011-11-02 00:59:12 +0200

    signup

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -15,8 +15,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title",
-                        :content => @base_title + " | Home")
+      response.should have_selector("title", :content => @base_title + " | Home")
     end
   end
 
@@ -28,8 +27,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title",
-                        :content => @base_title + " | Contact")
+      response.should have_selector("title", :content => @base_title + " | Contact")
     end
   end
 
@@ -41,8 +39,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title",
-                        :content => @base_title + " | About")
+      response.should have_selector("title", :content => @base_title + " | About")
     end
   end
 
@@ -54,8 +51,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'help'
-      response.should have_selector("title",
-                        :content => @base_title + " | Help")
+      response.should have_selector("title", :content => @base_title + " | Help")
     end
   end
 end
