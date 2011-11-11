ee5a0d700fc71dfc53a4e6b201d2a4b524d9899d
Alex SeNSe <sense8life@gmail.com>
2011-11-11 14:54:52 +0200

    11112011

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -85,4 +85,18 @@ describe User do
     end
   end
 
+  describe "password encryption" do
+
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
+    it "should set the encrypted password" do
+      @user.encrypted_password.should_not be_blank
+    end
+  end
+
 end
