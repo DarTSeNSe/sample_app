aea2a4c5a3e808193eb8eb8bb3e1e3442828a0ca
Alex SeNSe <sense8life@gmail.com>
2011-11-17 17:20:10 +0200

    17112011

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,15 @@ class UsersController < ApplicationController
     @title = "Sign Up"
   end
 
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
+
 end
