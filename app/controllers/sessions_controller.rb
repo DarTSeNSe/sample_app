3b1751579c0365020a61ab75a1fd3e4ffa87a265
Alex SeNSe <sense8life@gmail.com>
2011-11-24 16:59:26 +0200

    21112011

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-
+  #force_ssl :except => :destroy
   def new
     @title = "Sign in"
   end
@@ -21,6 +21,4 @@ class SessionsController < ApplicationController
     sign_out
     redirect_to root_path
   end
-
-
 end
